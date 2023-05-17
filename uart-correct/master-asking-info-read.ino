06312e0dffd311a653f7cca63ebbb9b515e8a9d3
Mônica Emediato <monicae@ufmg.br>
2023-05-17 11:01:45 -0300

    Update master-asking-info-read.ino

--- uart-correct/master-asking-info-read.ino
+++ uart-correct/master-asking-info-read.ino
@@ -13,25 +13,25 @@
 //{
 //
 //  //getting the voltage reading from the temperature sensor
-// int reading = analogRead(LM35);
-//
+// int reading = analogRead(LM35);  
+// 
 // // converting that reading to voltage, for 3.3v arduino use 3.3
 // float voltage = reading * 5.0;
-// voltage /= 1024.0;
-//
+// voltage /= 1024.0; 
+// 
 // // print out the voltage
 // Serial.print(voltage); Serial.println(" volts");
-//
+// 
 // // now print out the temperature
 // float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
 //                                               //to degrees ((voltage - 500mV) times 100)
 // Serial.print(temperatureC); Serial.println(" degrees C");
-//
+// 
 // // now convert to Fahrenheit
 // float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
 // Serial.print(temperatureF); Serial.println(" degrees F");
-//
-// delay(1000);
+// 
+// delay(1000);    
 //}
 
 //sensor de temperatura usando o LM35
@@ -39,7 +39,7 @@
 #define ledGreen 6
 
 int ledPin = 13; // internal LED is connected to built-in LED pin
-int ledState = LOW;
+int ledState = LOW; 
 
 int temp = 0 ; // variable to store temperature reading from slave
 
@@ -88,18 +88,18 @@ void setup()
   pinMode(ledPin, OUTPUT); // LED as OUTPUT
   pinMode(ledRed, OUTPUT);
   pinMode(ledGreen, OUTPUT);
-  pinMode(buttonPin, INPUT); //BUTTON INPUT
-
+  pinMode(buttonPin, INPUT); //BUTTON INPUT  
+  
   Serial.begin(9600);//inicializa a comunicacao serial
 }
 
 
 void blink_leds() {
-        previous_time = time;
+  	previous_time = time;
     time = millis();
-        if (time < previous_time)
+  	if (time < previous_time)
     {
-     return;
+     return; 
     }
     if (red_on && ((time - time_red_led >= blinkDuration) || time_red_led == 0) )
     {
@@ -109,7 +109,7 @@ void blink_leds() {
     } else {
         digitalWrite(ledRed, LOW);
     }
-        if (green_on && ((time - time_green_led >= blinkDuration) || time_green_led == 0) )
+ 	if (green_on && ((time - time_green_led >= blinkDuration) || time_green_led == 0) )
     {
         time_green_led = time;
         last_state_green_led = !last_state_green_led;
@@ -120,7 +120,7 @@ void blink_leds() {
         digitalWrite(ledGreen, LOW);
     }
 
-
+  
     /*if (green_on) {
         digitalWrite(ledGreen, (time / blinkDuration) % 2);
     } else {
@@ -131,8 +131,8 @@ void blink_leds() {
 
 int read_serial() {
     // clear the string for new input:
-        inString = "";
-        while (Serial.available() > 0) {
+  	inString = "";
+  	while (Serial.available() > 0) {
       int inChar = Serial.read();
       if (isDigit(inChar)) {
         // convert the incoming byte to a char and add it to the string:
@@ -144,7 +144,7 @@ int read_serial() {
         Serial.println(inString.toInt());
 
         return inString.toInt();
-
+            
     }
   }
 }
@@ -152,27 +152,27 @@ int read_serial() {
 void loop() {
   blink_leds();
   digitalWrite(ledPin, LOW);
-
+  
   int reading = digitalRead(buttonPin);
   //temp =0;
-
+   
   //BUTTON DEBOUNCING
   if (reading != lastButtonState) {
     // reset the debouncing timer
     lastDebounceTime = millis();
   }
-
+    
   if ((millis() - lastDebounceTime) > debounceDelay) {
-        // if the button state has changed:
+  	// if the button state has changed:
     if (reading != buttonState) {
       buttonState = reading;
 
       // only toggle the LED if the new button state is HIGH
       if (buttonState == HIGH) {
 
-        //write it down in the slave serial
-        Serial.println('o');
-      }
+        //write it down in the slave serial 
+      	Serial.println('o'); 
+      } 
       else {
         temp = read_serial();
 
@@ -181,11 +181,11 @@ void loop() {
         Serial.println(temp);
       }
     }
-  }
+  } 
 
 
   if (int(temp) > 25) {
-        //pinState = !pinState; // inverter estad
+  	//pinState = !pinState; // inverter estad
     digitalWrite(ledPin, HIGH);
     //Serial.println(temp);
   }
@@ -195,11 +195,11 @@ void loop() {
     red_on = false;
     digitalWrite(ledPin, pinState);
   }
-  else if (temp > 35){
+  else if (temp > 35){  
     green_on = false;
     red_on = true;
   }
-
+  
   // save the reading. Next time through the loop, it'll be the lastButtonState:
   lastButtonState = reading;
 }
